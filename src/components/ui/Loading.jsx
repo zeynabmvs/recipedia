c4bf2dbdf780e2f2bb5c654026ca60b375c28d47
zeynab.mvs <zeynab.mvs@gmail.com>
2025-01-12 18:06:59 +0330

    ui fix

--- src/components/ui/Loading.jsx
+++ src/components/ui/Loading.jsx
@@ -1,9 +1,9 @@
 export default function Loading() {
     return (
       <div className="flex items-center justify-center space-x-2">
-        <div className="h-5 w-5 animate-bounce rounded-full bg-red-500 [animation-delay:-0.3s]"></div>
-        <div className="h-5 w-5 animate-bounce rounded-full bg-red-500 [animation-delay:-0.13s]"></div>
-        <div className="h-5 w-5 animate-bounce rounded-full bg-red-500"></div>
+        <div className="h-5 w-5 animate-bounce rounded-full bg-primary [animation-delay:-0.3s]"></div>
+        <div className="h-5 w-5 animate-bounce rounded-full bg-primary [animation-delay:-0.13s]"></div>
+        <div className="h-5 w-5 animate-bounce rounded-full bg-primary"></div>
       </div>
     )
   }
