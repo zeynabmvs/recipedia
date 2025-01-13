a35237e1926b3158746ad7e0f636187ae5637a4b
zeynab.mvs <zeynab.mvs@gmail.com>
2025-01-13 15:26:44 +0330

    Improve 404 page ui

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
-    <>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for could not be found.</p>
-    </>
+    <div className="z-container min-h-full text-center py-20 md:py-40">
+      <h1 className="text-display-1 mb-4">404 - Page Not Found</h1>
+      <p className="text-body-lg">
+        Sorry, the page you are looking for could not be found. <Link to='/recipes' className="link text-primary">Browse recipes</Link>
+      </p>
+      
+    </div>
   );
 };
 
