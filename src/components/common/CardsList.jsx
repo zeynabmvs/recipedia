accd67614fb13d0cf5e9645d01d901e41e54b222
zeynab.mvs <zeynab.mvs@gmail.com>
2025-01-18 16:51:15 +0330

    add frameer animation to cardsList cards

--- src/components/common/CardsList.jsx
+++ src/components/common/CardsList.jsx
@@ -1,9 +1,10 @@
+import { motion } from "framer-motion";
 import Card from "components/common/Card";
 import Error from "components/common/Error";
 import Loading from "components/ui/Loading";
 import { DEFAULT_PER_PAGE } from "src/data";
 
-// use count prop with caution, if set different than default_per_page it may lead to miscalculation in pagination
+// use count prop with caution, if set different than DEFAULT_PER_PAGE it may lead to miscalculation in pagination
 function CardsList({
   list,
   error = "",
@@ -11,6 +12,19 @@ function CardsList({
   className = "",
   count = DEFAULT_PER_PAGE,
 }) {
+  const cardVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: (index) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: index * 0.1,
+        duration: 0.75,
+        type: "spring",
+      },
+    }),
+  };
+
   if (loading) {
     return <Loading />;
   }
@@ -22,9 +36,17 @@ function CardsList({
     <div
       className={` ${className} grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-8 `}
     >
-      {list.slice(0, count).map((item, index) => {
-        return <Card key={index} recipe={item}></Card>;
-      })}
+      {list.slice(0, count).map((item, index) => (
+        <motion.div
+          key={index}
+          custom={index}
+          variants={cardVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <Card recipe={item} />
+        </motion.div>
+      ))}
     </div>
   ) : (
     <p className="text-center py-10">Nothing Found...</p>
