730cc2aa65d19ee352f60574a144b4d8cd0907a3
zeynab.mvs <zeynab.mvs@gmail.com>
2025-01-25 17:07:20 +0330

    refactor: simplify pagination logic by removing useEffect

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,17 +1,18 @@
 import { DEFAULT_PER_PAGE } from "src/data";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const usePagination = (items = [], perPage = DEFAULT_PER_PAGE) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [paginatedList, setPaginatdList] = useState([]);
-  
-  useEffect(() => {
+
+  const getPaginatedList = () => {
     if (items?.length > 0) {
       const indexOfLastPost = currentPage * perPage;
       const indexOfFirstPost = indexOfLastPost - perPage;
-      setPaginatdList(items?.slice(indexOfFirstPost, indexOfLastPost));
+      return items?.slice(indexOfFirstPost, indexOfLastPost);
     }
-  }, [items, perPage, currentPage]);
+  };
+
+  const paginatedList = getPaginatedList();
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
