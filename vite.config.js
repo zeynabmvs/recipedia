90e4f9b95c7a28b30569e0712947ab6d7f82b75f
zeynab.mvs <zeynab.mvs@gmail.com>
2025-01-31 17:43:57 +0330

    audit some packages, change vite config for tailwind migration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       src: "/src",
